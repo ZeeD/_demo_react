2b90aab71038e105b8f0df21d4a32f58bf839ffa
Vito De Tullio <vito.detullio@gmail.com>
2021-06-23 10:06:37 +0200

    Component withStyles

--- src/partial_state/Section.jsx
+++ src/partial_state/Section.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
+import sleep from 'sleep-promise';
+import { withStyles } from '@material-ui/core/styles';
+
 import { equals } from '../common/utils';
 
 
-export default class Section extends Component {
+const styles = {
+    dlLoading: {
+        background: 'red',
+    },
+    dl: {
+        background: 'green'
+    }
+};
+
+
+export default withStyles(styles)(class Section extends Component {
     constructor(props) {
         super(props);
         this.state = {};
@@ -25,15 +38,25 @@ export default class Section extends Component {
         if (this.state[this.props.match.params.id])
             return;
 
-        fetch(`/partial_state_${this.props.match.params.id}.json`)
-            .then(response => response.json())
-            .then(value => this.setState({
-                [this.props.match.params.id]: value
-            }));
+        this.setState({
+            loading: true
+        }, () => sleep(2000)
+            .then(() => fetch(`/partial_state_${this.props.match.params.id}.json`)
+                .then(response => response.json())
+                .then(value => this.setState({
+                    [this.props.match.params.id]: value
+                })))
+            .then(() => this.setState({
+                loading: false
+            })));
     }
 
     render() {
-        return <dl>
+        const { classes } = this.props;
+
+        const dlClassName = this.state.loading ? classes.dlLoading : classes.dl;
+
+        return <dl className={dlClassName}>
             <dt>id</dt>
             <dd>{this.props.match.params.id}</dd>
             <dt>payload</dt>
@@ -42,4 +65,4 @@ export default class Section extends Component {
             <dd>{JSON.stringify(this.state)}</dd>
         </dl>;
     }
-};
+});
