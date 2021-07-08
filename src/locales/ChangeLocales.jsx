e09e6a5b5c632d77316d6dbc7af71e73e35751e8
Vito De Tullio <vito.detullio@gmail.com>
2021-07-08 09:45:34 +0200

    stuff

--- src/locales/ChangeLocales.jsx
+++ src/locales/ChangeLocales.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { addLocale, useLocale } from 'ttag';
+import { t, addLocale, useLocale } from 'ttag';
 
 import StateContext from '../common/statecontext/StateContext';
 
@@ -28,23 +28,34 @@ export default class Locales extends Component {
 
     render() {
         const { setState } = this.context;
-        const setLocale = currentLocale => {
-            setState({ currentLocale }, _ => useLocale(currentLocale));
-        };
+        const setLocale = currentLocale => setState({
+            currentLocale,
+            loading: true
+        },
+            _ => {
+                useLocale(currentLocale);
+                setState({ loading: false });
+            });
 
         const buttons = [];
         for (const localeName of ['en', 'it', 'example']) {
             const button = (
                 <button onClick={setLocale.bind(null, localeName)}
-                        key={localeName}>
+                    key={localeName}>
                     {localeName}
                 </button>
             );
             buttons.unshift(button);
         }
 
+        const name = 'pippo';
+
         return <>
-            {buttons}
+            <ul>
+                <li>{t`Titolo, personaggio, genere..`}</li>
+                <li>{t`Ciao ${name}`}</li>
+            </ul>
+            <div>{buttons}</div>
         </>;
     }
 }
